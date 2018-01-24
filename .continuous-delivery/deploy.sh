0892fe99107f3ad77834635db198b3a2959f6cdc
Tamazlykar Daniil <tadani7@gmail.com>
2018-01-24 13:03:58 +0700

    Fix path to k8s files

--- .continuous-delivery/deploy.sh
+++ .continuous-delivery/deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+K8S_DEPLOYMENT_PATH=./.continuous-delivery/k8s/deployment.yaml
+K8S_SERVICE_PATH=./.continuous-delivery/k8s/service.yaml
+
 # Get crediantials and auth to gcloud
 echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > $GOOGLE_APPLICATION_CREDENTIALS
 gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
@@ -16,11 +19,11 @@ docker build -t gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT} .
 gcloud docker -- push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
 
 # Change image in deployments.yaml
-sed -i "s#gcr.io#gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}#g" ./k8s/deployment.yaml
+sed -i "s#gcr.io#gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}#g" $K8S_DEPLOYMENT_PATH
 
 # Create/update deployment and service on k8s
-kubectl apply -f ./.continuous-delivery/k8s/deployment.yaml
-kubectl apply -f ./.continuous-delivery/k8s/service.yaml
+kubectl apply -f $K8S_DEPLOYMENT_PATH
+kubectl apply -f $K8S_SERVICE_PATH
 
 # Print application URL
 echo http://`kubectl get service/average-salary --output=json | jq -r '.status.loadBalancer.ingress[0].ip'`
